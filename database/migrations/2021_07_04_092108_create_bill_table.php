cee7ed44553bd71836a5f59f27b70cc275b4eeb4
tran khac nam <nam3061998@gmail.com>
2021-07-08 17:02:31 +0700

    init database

---
+++ database/migrations/2021_07_04_092108_create_bill_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_order');
+            $table->integer('total');
+            $table->string('payment');
+            $table->string('note');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
